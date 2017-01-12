8cf6b39b07a81f2246a7ab427104ff86aa2c80fb
Mark Mullan <markrmullan@gmail.com>
2017-01-12 11:01:21 -0800

    reduce sleep, fix developer pay, better documentation

--- orders_seeder.py
+++ orders_seeder.py
@@ -1,11 +1,3 @@
-import requests
-import json
-from random import randint
-import sys
-from time import sleep
-from Crypto.PublicKey import RSA
-from base64 import b64encode
-
 #######################################
 ############ GENERAL USAGE ############
 #######################################
@@ -24,7 +16,7 @@ To ensure payments are made in full, turn off all default taxes.
 
 MID = ""
 API_TOKEN = ""
-NUM_ORDERS = 777
+NUM_ORDERS = 50
 ENVIRONMENT = "https://sandbox.dev.clover.com/"
 
 #######################################
@@ -36,11 +28,21 @@ ENVIRONMENT = "https://sandbox.dev.clover.com/"
 ######################################
 cardNumber = '4761739001010010'
 expMonth = 12
-expYear = 2018
+expYear = 2020
 CVV = None
 ######################################
 ######################################
 
+import requests
+import json
+from random import randint
+import sys
+from time import sleep
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from base64 import b64encode
+
+# Fetch all items from a merchant
 itemIds = []
 url = ENVIRONMENT + "v3/merchants/" + MID + "/items"
 headers = {'Authorization': 'Bearer ' + API_TOKEN}
@@ -55,8 +57,31 @@ for i in xrange(0, len(elements)):
     itemIds.append(str(elements[i][u'id']))
 
 num_items = len(itemIds)
+if (num_items == 0):
+    print "This merchant has no inventory. Create items and then re-run this script."
+    sys.exit()
+
+# Fetch developer pay secrets from GET /v2/merchant/{mId}/pay/key
+url = ENVIRONMENT + "v2/merchant/" + MID + '/pay/key'
+headers = {"Authorization": "Bearer " + API_TOKEN}
+response = requests.get(url, headers = headers)
+if response.status_code != 200:
+    print "Something went wrong fetching Developer Pay API secrets"
+    sys.exit()
+response = response.json()
+
+modulus = long(response['modulus'])
+exponent = long(response['exponent'])
+prefix = str(response['prefix'])
+
+RSAkey = RSA.construct((modulus, exponent))
+
+# helper function
+def print_progress(i):
+    print (str((float(i) / NUM_ORDERS) * 100)) + "% complete"
+
 for i in xrange(0, NUM_ORDERS):
-    sleep(0.4)
+    sleep(0.1)
     rand_item_index = randint(0, num_items - 1)
     url = ENVIRONMENT + "v3/merchants/" + MID + "/orders"
     response = requests.post(url, headers=headers, json={"state": "open"})
@@ -64,7 +89,7 @@ for i in xrange(0, NUM_ORDERS):
         print "Something went wrong creating an order"
         sys.exit()
     orderId = response.json()[u'id']
-    sleep(0.4)
+    sleep(0.1)
     url = ENVIRONMENT + "v3/merchants/" + MID + "/orders/" + orderId + "/line_items"
     payload = { "item": { "id": itemIds[rand_item_index] } }
     response = requests.post(url, headers=headers, json=payload)
@@ -75,24 +100,12 @@ for i in xrange(0, NUM_ORDERS):
     price = response.json()[u'price']
 
     ########## BEGIN PAYMENT ##########
-    sleep(0.4)
-    url = ENVIRONMENT + "v2/merchant/" + MID + '/pay/key'
-    headers = {"Authorization": "Bearer " + API_TOKEN}
-    response = requests.get(url, headers = headers)
-    if response.status_code != 200:
-        print "Something went wrong adding a line item to the order"
-        sys.exit()
-    response = response.json()
+    # create a cipher from the RSA key and use it to encrypt the card number, prepended with the prefix from GET /v2/merchant/{mId}/pay/key
+    cipher = PKCS1_OAEP.new(RSAkey)
+    encrypted = cipher.encrypt(prefix + cardNumber)
 
-    modulus = long(response['modulus'])
-    exponent = long(response['exponent'])
-    prefix = long(response['prefix'])
-
-    RSAkey = RSA.construct((modulus, exponent))
-
-    publickey = RSAkey.publickey()
-    encrypted = publickey.encrypt(cardNumber, prefix)
-    cardEncrypted = b64encode(encrypted[0])
+    # Base64 encode the resulting encrypted data into a string to use as the cardEncrypted' property.
+    cardEncrypted = b64encode(encrypted)
 
     post_data = {
         "orderId": orderId,
@@ -107,7 +120,7 @@ for i in xrange(0, NUM_ORDERS):
     }
 
     posturl = ENVIRONMENT + "v2/merchant/" + MID + '/pay'
-    sleep(0.4)
+    sleep(0.1)
     response = requests.post(
         posturl,
         headers = headers,
@@ -119,10 +132,10 @@ for i in xrange(0, NUM_ORDERS):
 
     url = ENVIRONMENT + "v3/merchants/" + MID + "/orders/" + orderId
     payload = {"total": long(price)}
-    sleep(0.4)
+    sleep(0.1)
     response = requests.post(url=url, headers=headers, json=payload)
     if response.status_code != 200:
         print "Something went wrong updating order total"
         sys.exit()
 
-    print str((float(i) / NUM_ORDERS) * 100) + "% complete"
+    print_progress(i)
